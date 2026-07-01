697e6ee05d9d491835216fb1f6c0ec53591d1475
Gungun786 <2k24.csai1b.2411918@gmail.com>
2026-07-01 20:50:37 +0530

    commit7

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
 import App from './App.jsx'
+
+// Fix Leaflet default icon paths for Vite / React builds
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
